a1f6f5fa57d9516de3f4caabc6226b26a3ce25ff
stevejobsii <stevejobsii@163.com>
2015-08-28 18:20:31 +0800

    search tag
    search tag

--- app/Http/Controllers/TagsController.php
+++ app/Http/Controllers/TagsController.php
@@ -2,19 +2,24 @@
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
-
+use Illuminate\Http\Request as urlRequest;
 use Illuminate\Http\Request;
 
 use App\Tag;
 
 class TagsController extends Controller {
 
-	public function show(Tag $tag)
+	public function show(Tag $tag,urlRequest $request)
 	{
+		//query within tag
+        if($search = $request->query('q'))
+        {
+        $articles = $tag->articles()->search($search)->orderBy('created_at', 'desc')->paginate(30);
+        }else{
 		//获取这个tag的articles并用articles.index反应
-		$articles = $tag->articles()->latest()->published()->paginate(10);
-
-		return view('articles.index', compact('articles'));
+		$articles = $tag->articles()->latest()->published()->paginate(10);}
+        $articles->setPath('articles');
+		return view('articles.index', compact('articles','search'));
 	}
 
 }
