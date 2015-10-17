f8ab7d4f19ef8e42a9ccbcdcd16025df769f39a3
stevejobsii <stevejobsii@163.com>
2015-10-17 17:25:08 +0800

    cdnn

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,8 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">	
-	<link  rel="stylesheet" href="/css/all.css">
+	<link href="//cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+	<link href="//cdn.bootcss.com/sweetalert/1.1.0/sweetalert.min.css" rel="stylesheet">
 	<link  rel="stylesheet" href="/css/main.css">
 	<meta id="token" name="token" value="{{ csrf_token() }}">
 
