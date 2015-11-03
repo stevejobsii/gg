9fa5f400b3956de06245cb99d16cecac898f8bc1
stevejobsii <stevejobsii@163.com>
2015-11-03 14:55:56 +0800

    nav strong

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -5,28 +5,28 @@
     </div>
     <div id="navbar" class="collapse navbar-collapse">
       <ul class="nav navbar-nav hoverwhite">
-           <li><a href="{{ url('articles') }}"><strong><i class="glyphicon glyphicon-picture" aria-hidden="true"></i>&nbsp;&nbsp;热门图片</strong></a></li>
-           <li><a href="{{ url('articles/create') }}"><strong><i class="glyphicon glyphicon-pencil" aria-hidden="true"></i>&nbsp;&nbsp;投稿</strong></a></li>
-           <li><a href="{{ url('guestbook') }}"><strong><i class="glyphicon glyphicon-comment" aria-hidden="true"></i>&nbsp;&nbsp;留言板</strong></a></li>
+           <li><a href="{{ url('articles') }}"><i class="glyphicon glyphicon-picture" aria-hidden="true"></i>&nbsp;&nbsp;热门图片</a></li>
+           <li><a href="{{ url('articles/create') }}"><i class="glyphicon glyphicon-pencil" aria-hidden="true"></i>&nbsp;&nbsp;投稿</a></li>
+           <li><a href="{{ url('guestbook') }}"><i class="glyphicon glyphicon-comment" aria-hidden="true"></i>&nbsp;&nbsp;留言板</a></li>
            <li class="dropdown ">
-             <a href="javascript:void(0)" class="dropdown-toggle" data-toggle="dropdown"><strong><i class="glyphicon glyphicon-tags" aria-hidden="true"></i>&nbsp;&nbsp;标签</strong> <span class="caret"></span></a>
+             <a href="javascript:void(0)" class="dropdown-toggle" data-toggle="dropdown"><i class="glyphicon glyphicon-tags" aria-hidden="true"></i>&nbsp;&nbsp;标签<span class="caret"></span></a>
                <ul class="dropdown-menu" role="menu">           
                <li >
-               <a href="{{ url('tags/China') }}"><strong>国内图片</strong></a>
+               <a href="{{ url('tags/China') }}">国内图片</a>
                </li>     
                <li >
-               <a href="{{ url('tags/Asia') }}"><strong>亚洲图片</strong></a>
+               <a href="{{ url('tags/Asia') }}">亚洲图片</a>
                </li>
                <li >
-               <a href="{{ url('tags/EU') }}"><strong>欧美图片</strong></a>
+               <a href="{{ url('tags/EU') }}">欧美图片</a>
                </li>                
                <li >
-               <a href="{{ url('tags/GIF') }}"><strong>GIF</strong></a>
+               <a href="{{ url('tags/GIF') }}">GIF</a>
                </li>  
                </ul>
            </li>
 
-           <li><a href="javascript:void(0)" class="dropdown-toggle" id="nav-search"><strong><i class="glyphicon glyphicon-search" aria-hidden="true"></i>&nbsp;&nbsp;搜索</strong> <span class="caret"></span></a>
+           <li><a href="javascript:void(0)" class="dropdown-toggle" id="nav-search"><i class="glyphicon glyphicon-search" aria-hidden="true"></i>&nbsp;&nbsp;搜索<span class="caret"></span></a>
            <div class = "nav-search">
            {!!Form::open(['method'=>'GET','class'=>'inner-addon  form-control-nav right-addon'])!!}
               <i class="glyphicon glyphicon-search"aria-hidden="true"></i>
@@ -60,29 +60,29 @@
          <ul class="dropdown-menu" role="menu">
          <li><a href="/users/{{Auth::user()->id}}/articles" >
          <span class="glyphicon glyphicon-inbox" aria-hidden="true"></span>
-         <strong>我的收藏</strong></a></li>
+         我的收藏</a></li>
          <li class="divider"></li> 
   
          <li><a href="/settings" >
          <span class="glyphicon glyphicon-wrench" aria-hidden="true"></span>
-         <strong>我的设置</strong></a></li>
+         我的设置</a></li>
          <li class="divider"></li> 
 
          <li><a id="bookmark-link" href="javascript:void(0)">
          <span class="glyphicon glyphicon-bookmark"aria-hidden="true"></span>
-         <strong>续看书签</strong></a></li>
+         续看书签</a></li>
          
          <li class="divider"></li>
          <li><a href="/auth/logout"><span class="glyphicon glyphicon-remove"aria-hidden="true"></span>
-         <strong>退出</strong></a></li>
+         退出</a></li>
          </ul>
          </li>
         
         @else        
          <li><a href="javascript:void(0)" data-toggle="modal" data-target="#myModalregister"><span class="glyphicon glyphicon-edit"aria-hidden="true"></span>
-         <strong>简单注册</strong></a></li>
+         简单注册</a></li>
          <li><a href="javascript:void(0)" data-toggle="modal" data-target="#myModallogin"><span class="glyphicon glyphicon-check"aria-hidden="true"></span>
-         <strong>登陆</strong></a></li>
+         登陆</a></li>
         @endif
       </ul>
     </div>
@@ -121,11 +121,11 @@
                         <button type="submit" class="btn btn-primary btn-lg" style="margin-right: 15px;">
                           登陆
                         </button>
-                        <strong>
+                        
                         &nbsp;
                         <a href="/auth/register">简单注册</a>
                         &nbsp;&nbsp; • &nbsp;&nbsp;
-                        <a href="/password/email">忘记密码？</a></strong>
+                        <a href="/password/email">忘记密码？</a><
                     </div>
                   </form>
         </div>
