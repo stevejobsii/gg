f796def3bbb97f36666d4cb78b81cb486e02a847
stevejobsii <stevejobsii@163.com>
2015-11-05 20:07:35 +0800

    css all

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,10 +5,9 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">	
     <meta id="token" name="token" value="{{ csrf_token() }}">
-    <meta name="keywords" content="搞笑 幽默 好去处 goodgoto 游戏 女性 恶搞 gif 笑笑小电影">
+    <meta name="keywords" content="无厘头 搞笑 幽默 好去处 goodgoto 游戏 女性 恶搞 gif 笑笑小电影">
     <meta name="baidu_union_verify" content="3c05d5a3879ebffd964063aada5f8721">
-	<link href="//cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
-	<link href="//cdn.bootcss.com/sweetalert/1.1.0/sweetalert.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="/css/all.css">
 	<link rel="stylesheet" href="/css/main.css">
     <link rel="shortcut icon" href="/images/catalog/goodgotologo.jpg">
     <title>GoodGoto好去处</title>
@@ -29,9 +28,7 @@
 		@yield('content')
 	</div> 
 	<p id="back-to-top" ><a href="javascript:void(0)">回到顶部</a></p>
-
     <script src="/js/all.js"></script>
-
 	@yield('footer')
 	@yield('js')
 	@include('flash')
