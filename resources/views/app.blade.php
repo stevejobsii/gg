8ffde167a2efd8364f067eede4fd9793d440ad28
stevejobsii <stevejobsii@163.com>
2015-11-22 15:10:55 +0800

    g

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -28,9 +28,12 @@
 	<div class="container page">
 		@yield('content')
 		<ins class="adsbygoogle"
-		     style="display:inline-block;width:100%;height:600px"
+		     style="display:inline-block;width:300px;height:600px"
 		     data-ad-client="ca-pub-9854929025598162"
 		     data-ad-slot="9917246736"></ins>
+		     <script>
+		(adsbygoogle = window.adsbygoogle || []).push({});
+	</script>
 	</div> 
 	<p id="back-to-top" ><a href="javascript:void(0)"></a></p>
     <script src="/js/all.js"></script>
