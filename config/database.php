9e54a7b951dc92d0bf087affad6ca389f0f0f27f
stevejobsii <stevejobsii@163.com>
2015-11-27 15:18:52 +0800

    port

--- config/database.php
+++ config/database.php
@@ -62,6 +62,7 @@ return [
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
             'strict'    => false,
+            'port'      => '33060',
         ],
 
         'pgsql' => [
