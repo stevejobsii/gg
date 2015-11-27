6ad4eaa74bc920e5c06f4494bcc47ece464f6f4b
stevejobsii <stevejobsii@163.com>
2015-11-27 20:08:27 +0800

    test

--- config/database.php
+++ config/database.php
@@ -54,7 +54,7 @@ return [
 
         'mysql' => [
             'driver'    => 'mysql',
-            'host' => env('DB_HOST', 'localhost') . ('homestead' == gethostname() ? null : ':33060'),
+            'host'      => env('DB_HOST'),
             'database'  => env('DB_DATABASE'),
             'username'  => env('DB_USERNAME'),
             'password'  => env('DB_PASSWORD'),
