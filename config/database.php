8cffa8dfd2395683ce5b58d92d09738b90be5cf4
stevejobsii <stevejobsii@163.com>
2015-11-27 20:36:15 +0800

    test

--- config/database.php
+++ config/database.php
@@ -62,7 +62,6 @@ return [
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
             'strict'    => false,
-            'port'      => '3306',
         ],
 
         'pgsql' => [
