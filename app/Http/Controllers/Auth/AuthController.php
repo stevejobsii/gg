b37bf8b5672c58ef52d8619b57243d3c054c0fce
stevejobsii <stevejobsii@163.com>
2015-11-28 11:25:19 +0800

    test

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -84,7 +84,7 @@ class AuthController extends Controller
             ->resize(31, 31)
             ->encode('jpg')
             ->save(base_path() . '/public/images/avatar/30avatar' . $user->id . '.jpg');
-        return $oauthUser;
+        dd($oauthUser);
         // var_dump($oauthUser->getId());
         // var_dump($oauthUser->getNickname());
         // var_dump($oauthUser->getName());
