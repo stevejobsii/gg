8eefba95a42fcce3a0b86115548df62500c1b03b
stevejobsii <stevejobsii@163.com>
2015-11-28 20:35:48 +0800

    time

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -81,11 +81,9 @@ class AuthController extends Controller
         if ($user->avatar == '/images/avatar/avatardefault.jpg'){
         Image::make($oauthUser->avatar)
             ->resize(100, 100)
-            ->encode('jpg')
             ->save(base_path() . '/public/images/avatar/avatar' . $user->id . '.jpg');
         Image::make($oauthUser->avatar)
             ->resize(31, 31)
-            ->encode('jpg')
             ->save(base_path() . '/public/images/avatar/30avatar' . $user->id . '.jpg');
         $user->avatar = '/images/avatar/avatar' . $user->id . '.jpg';
         $user->save();}
