fb55a89b9aea338dbca7e38613ce125b3ccf0121
di <stevejobsii@outlook.com>
2016-04-22 09:55:08 +0800

    logo

--- resources/views/chen.blade.php
+++ resources/views/chen.blade.php
@@ -21,7 +21,7 @@
             margin:0 auto;
         }
         #title{
-        	margin-top: 40px;
+        	margin-top: 50px;
         	text-align: center;
         	color: rgb(43,34,125);
         }
@@ -51,43 +51,22 @@
 		    height: 25px;
 		    line-height: 25px;
         }
-		.link{
-		    border: 5px solid #f5da55;
-		    border-radius: 50px;
-		    width: 70px;
+        #title img{
+        	max-width: 90%;
+        	max-height: 90%;
+        }
+        #first-layer {
+        	margin-top: 50px;
+        }
+        #first-layer img{
+        	width: 70px;
 		    height: 70px;
-		    line-height: 60px;
-		    color: #f5da55;
-		    font-size: 40px;
-		    font-weight: 400;
-		    opacity: 1;
-		    text-align: center;
-			color: white;
-			cursor: pointer;
-			margin-left: auto;
-			margin-right: auto;
-		}
-		.link1{
-			background: rgb(7,0,0);
-		}
-		.link2{
-			background: rgb(43,34,125);
-		}
-		.link3{
-			background: rgb(247,216,195);
-		}
-		.link4{
-			background: rgb(250,162,90);
-		}
-		.link5{
-			background: rgb(143,36,62);
-		}
-		.link6{
-			background: rgb(145,94,161);
-		}
-		.link7{
-			background: rgb(181,176,208);
-		}
+        }
+
+        #second-layer img{
+        	width: 70px;
+		    height: 70px;
+        }
 		.link-1{
 			color: rgb(43,34,125);
 			width: 70px;
@@ -144,33 +123,33 @@
 </head>
 	<body id="body">
 	    <div id="title">
-	      <h2>全国卫生计生系统</h2><h1>摄影大赛</h1><div class="new">最新</div>
+	      <img src="/images/title.png"><div class="new">最新</div>
 	    </div>
 		<div class="wrap">
 		    <div class="container">
 			    <div id="first-layer">
 				    <div class="col-md-4 col-sm-4 col-xs-4">
-	                    <i class="link link1 glyphicon glyphicon-envelope" aria-hidden="true"></i><p class="link-1">大赛通知</p>
+	                    <img src="/images/i1.png"><p class="link-1">大赛通知</p>
 				    </div>
 			        <div class="col-md-4 col-sm-4 col-xs-4">
-				        <i class="link link2 glyphicon glyphicon-camera" aria-hidden="true"></i><p class="link-2">摄影投稿</p>
+				        <img src="/images/i2.png"><p class="link-2">摄影投稿</p>
 				    </div>	   
 				    <div class="col-md-4 col-sm-4 col-xs-4">
-				        <i class="link link3 glyphicon glyphicon-facetime-video" aria-hidden="true"></i><p class="link-3">媒体登记</p>
+				        <img src="/images/i3.png"><p class="link-3">媒体登记</p>
 				    </div>
 				</div>
 				<div id="second-layer">
 				    <div class="col-md-3 col-sm-3 col-xs-3">
-	                    <i class="link link4 glyphicon glyphicon-picture" aria-hidden="true"></i><p class="link-4">作品展示</p>
+	                    <img src="/images/i4.png"><p class="link-4">作品展示</p>
 				    </div>
 			        <div class="col-md-3 col-sm-3 col-xs-3">
-				        <i class="link link5 glyphicon glyphicon-knight" aria-hidden="true"></i><p class="link-5">省赛区</p>
+				        <img src="/images/i5.png"><p class="link-5">省赛区</p>
 				    </div>	   
 				    <div class="col-md-3 col-sm-3 col-xs-3">
-				        <i class="link link6 glyphicon glyphicon-king" aria-hidden="true"></i><p class="link-6">总决赛</p>
+				        <img src="/images/i6.png"><p class="link-6">总决赛</p>
 				    </div>
 				    <div class="col-md-3 col-sm-3 col-xs-3">
-                        <i class="link link7 glyphicon glyphicon-grain" aria-hidden="true"></i><p class="link-7">摄影培训</p>
+                        <img src="/images/i7.png"><p class="link-7">摄影培训</p>
 				    </div>
 				</div>
 		    </div>
