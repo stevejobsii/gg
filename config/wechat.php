25dd41a766956dab7a60b6b40a239a884e1c0f64
di <stevejobsii@outlook.com>
2016-04-30 21:05:56 +0800

    test

--- config/wechat.php
+++ config/wechat.php
@@ -16,9 +16,9 @@ return [
     /**
      * 账号基本信息，请从微信公众平台/开放平台获取
      */
-    'app_id'  => env('WECHAT_APPID', 'your-app-id'),         // AppID
-    'secret'  => env('WECHAT_SECRET', 'your-app-secret'),     // AppSecret
-    'token'   => env('WECHAT_TOKEN', 'your-token'),          // Token
+    'app_id'  => env('WECHAT_APPID'),         // AppID
+    'secret'  => env('WECHAT_SECRET'),     // AppSecret
+    'token'   => env('WECHAT_TOKEN'),          // Token
     'aes_key' => env('WECHAT_AES_KEY', ''),                    // EncodingAESKey
 
     /**
