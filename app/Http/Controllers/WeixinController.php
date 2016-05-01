b0851ad1917ee9b9e84ebfa6f0585ad38e63e125
di <stevejobsii@outlook.com>
2016-05-01 21:08:17 +0800

    test

--- app/Http/Controllers/WeixinController.php
+++ app/Http/Controllers/WeixinController.php
@@ -19,38 +19,33 @@ class WeixinController extends Controller
             
         });
         
-        $menu = $wechat->menu;
-        $menus = $menu->current();
-        $buttons = [
-            [
-                "type" => "click",
-                "name" => "今日歌曲",
-                "key"  => "V1001_TODAY_MUSIC"
-            ],
-            [
-                "name"       => "菜单",
-                "sub_button" => [
-                    [
-                        "type" => "view",
-                        "name" => "搜索",
-                        "url"  => "http://www.soso.com/"
-                    ],
-                    [
-                        "type" => "view",
-                        "name" => "视频",
-                        "url"  => "http://v.qq.com/"
-                    ],
-                    [
-                        "type" => "click",
-                        "name" => "赞一下我们",
-                        "key" => "V1001_GOOD"
-                    ],
-                ],
-            ],
-        ];
-        $menu->add($buttons);
-        Log::info('return response.');
-        return $wechat->server->serve();
+        // $menu = $wechat->menu;
+        // $menus = $menu->current();
+        // $buttons = [
+        //     [
+        //         "type" => "click",
+        //         "name" => "今日歌曲",
+        //         "key"  => "V1001_TODAY_MUSIC"
+        //     ],
+        //     [
+        //         "name"       => "菜单",
+        //         "sub_button" => [
+        //             [
+        //                 "type" => "view",
+        //                 "name" => "搜索",
+        //                 "url"  => "http://www.soso.com/"
+        //             ],
+        //             [
+        //                 "type" => "view",
+        //                 "name" => "视频",
+        //                 "url"  => "http://v.qq.com/"
+        //             ],
+        //         ],
+        //     ],
+        // ];
+        // $menu->add($buttons);
+        // Log::info('return response.');
+        // return $wechat->server->serve();
     }
 
     public function demo1()
