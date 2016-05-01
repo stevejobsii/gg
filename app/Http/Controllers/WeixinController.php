114130caf8399a9d0d18a3ecfcc22286602ebda7
di <stevejobsii@outlook.com>
2016-05-01 14:44:27 +0800

    test

--- app/Http/Controllers/WeixinController.php
+++ app/Http/Controllers/WeixinController.php
@@ -11,7 +11,6 @@ use Wechat;
 
 class WeixinController extends Controller
 {
-
     public function serve()
     {
         Log::info('request arrived.'); 
@@ -23,76 +22,8 @@ class WeixinController extends Controller
         Log::info('return response.');
         return $server->serve();
     }
-
-
-    public function weixingame(Request $request)
-    {
-        return view('weixingame');
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+    // public function weixingame(Request $request)
+    // {
+    //     return view('weixingame');
+    // }
 }
