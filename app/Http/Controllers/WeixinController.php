9f56b375597b642bc69834bb276b589d8a7010bb
di <stevejobsii@outlook.com>
2016-05-01 20:55:55 +0800

    test

--- app/Http/Controllers/WeixinController.php
+++ app/Http/Controllers/WeixinController.php
@@ -13,12 +13,43 @@ class WeixinController extends Controller
     public function serve()
     {
         Log::info('request arrived.'); 
-        $server = app('wechat')->server;
-        $server->setMessageHandler(function($message){
+        $wechat = app('wechat');
+        $wechat->server->setMessageHandler(function($message){
             return "欢迎访问兔朱迪的窝！";
+            
         });
+        
+        $menu = $wechat->menu;
+        $buttons = [
+            [
+                "type" => "click",
+                "name" => "今日歌曲",
+                "key"  => "V1001_TODAY_MUSIC"
+            ],
+            [
+                "name"       => "菜单",
+                "sub_button" => [
+                    [
+                        "type" => "view",
+                        "name" => "搜索",
+                        "url"  => "http://www.soso.com/"
+                    ],
+                    [
+                        "type" => "view",
+                        "name" => "视频",
+                        "url"  => "http://v.qq.com/"
+                    ],
+                    [
+                        "type" => "click",
+                        "name" => "赞一下我们",
+                        "key" => "V1001_GOOD"
+                    ],
+                ],
+            ],
+        ];
+        $menu->add($buttons);
         Log::info('return response.');
-        return $server->serve();
+        return $wechat->server->serve()
     }
 
     public function demo1()
