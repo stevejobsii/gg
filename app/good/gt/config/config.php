0ac9027584225dd4d36b9eb89b470e24f9c48e2c
di <stevejobsii@outlook.com>
2016-05-02 14:11:22 +0800

    test

--- app/good/gt/config/config.php
+++ app/good/gt/config/config.php
@@ -4,7 +4,7 @@ namespace App\good\gt\config;
 *  Geetest配置文件
 * @author Tanxu
 */
-define("CAPTCHA_ID", "b46d1900d0a894591916ea94ea91bd2c");
-define("PRIVATE_KEY", "36fc3fe98530eea08dfc6ce76e3d24c4");
+define("CAPTCHA_ID", "3945d747904af599d23d2f287b4055c6");
+define("PRIVATE_KEY", "2388884d50fc63a350392eb1e9de1c6b");
 
  ?>
