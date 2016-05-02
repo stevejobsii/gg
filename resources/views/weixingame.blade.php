7052cdad5255dc747863ba2fe1e23c37276e23c5
di <stevejobsii@outlook.com>
2016-05-02 16:43:20 +0800

    test

--- resources/views/weixingame.blade.php
+++ resources/views/weixingame.blade.php
@@ -203,11 +203,10 @@ wx.config(<?php echo $js->config(array('onMenuShareQQ', 'onMenuShareWeibo','onMe
 wx.ready(function () {
 document.querySelector('#onMenuShareAppMessage').onclick = function () {
     wx.onMenuShareAppMessage({
-        title: '你能戳几下？', // 分享标题
-        link: "http://gdws.cn/2016gamecs/index.html", // 分享链接
-        imgUrl: 'http://gdws.cn/2016gamecs/index.html/public/images/screenshot.jpg', // 分享图标
-        desc: "戳了-999下！请您不要用脚点，用手行么！",
-        domain:"http://gdws.cn",
+       title: '互联网之子',
+      desc: '在长大的过程中，我才慢慢发现，我身边的所有事，别人跟我说的所有事，那些所谓本来如此，注定如此的事，它们其实没有非得如此，事情是可以改变的。更重要的是，有些事既然错了，那就该做出改变。',
+      link: 'http://movie.douban.com/subject/25785114/',
+      imgUrl: 'http://demo.open.weixin.qq.com/jssdk/images/p2166127561.jpg',
       trigger: function (res) {
         // 不要尝试在trigger中使用ajax异步请求修改本次分享的内容，因为客户端分享操作是一个同步操作，这时候使用ajax的回包会还没有返回
         alert('用户点击发送给朋友');
