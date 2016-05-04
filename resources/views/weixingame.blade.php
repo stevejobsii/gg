08e51b329f074492eed0253f148762f4209440fe
di <stevejobsii@outlook.com>
2016-05-04 10:19:00 +0800

    test

--- resources/views/weixingame.blade.php
+++ resources/views/weixingame.blade.php
@@ -209,7 +209,6 @@ document.querySelector('#onMenuShareAppMessage').onclick = function () {
       desc: '在长大的过程中，我才慢慢发现，我身边的所有事，别人跟我说的所有事，那些所谓本来如此，注定如此的事，它们其实没有非得如此，事情是可以改变的。更重要的是，有些事既然错了，那就该做出改变。',
       link: 'https://goodgoto.com',
       imgUrl: 'https://goodgoto.com/images/catalog/30avatardefault.jpg',
-      data_url	:	'',
       trigger: function (res) {
         // 不要尝试在trigger中使用ajax异步请求修改本次分享的内容，因为客户端分享操作是一个同步操作，这时候使用ajax的回包会还没有返回
         alert('用户点击发送给朋友');
