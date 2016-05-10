94ab167a8f0f1b02575c98c626dcc5ffc122a64c
di <stevejobsii@outlook.com>
2016-05-10 14:20:56 +0800

    cup

--- resources/views/h13/index.blade.php
+++ resources/views/h13/index.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="zh-CN">
 <head>
+<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
       <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <meta name="format-detection" content="telephone=yes">
