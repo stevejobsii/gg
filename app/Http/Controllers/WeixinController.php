e749f88b261ce1845d274b03fd4273927ddea1c0
di <stevejobsii@outlook.com>
2016-05-12 12:27:40 +0800

    test

--- app/Http/Controllers/WeixinController.php
+++ app/Http/Controllers/WeixinController.php
@@ -22,46 +22,46 @@ class WeixinController extends Controller
         $server->setMessageHandler(function($message) use ($userApi){
             switch($message->MsgType){
                 case'event':
-                if($message->Event=='subscribe'){
-                    return '欢迎'.$userApi->get($message->FromUserName)->nickname.'关注广州市浩立生物科技有限公司！';
-                }
-                if($message->Event=='CLICK'){
-                    switch($message->EventKey){
-                        case'hyjcy': 
-                            return '还原基础油';
-                            break;
-                        case'gchysz': 
-                            return '高纯环氧树脂';
-                            break;
-                        case'gcdgz': 
-                            return '高纯单甘酯';
-                            break;
-                        // case'hyjcy': 
-                        //     return '还原基础油';
-                        //     break;
-                        // case'hyjcy': 
-                        //     return '还原基础油';
-                        //     break;
-                        // case'hyjcy': 
-                        //     return '还原基础油';
-                        //     break;
-                        // case'hyjcy': 
-                        //     return '还原基础油';
-                        //     break;
-                        // case'hyjcy': 
-                        //     return '还原基础油';
-                        //     break;
+                    if($message->Event=='subscribe'){
+                        return '欢迎'.$userApi->get($message->FromUserName)->nickname.'关注广州市浩立生物科技有限公司！';
                     }
-
-                }
+                    if($message->Event=='CLICK'){
+                        switch($message->EventKey){
+                            case'hyjcy': 
+                                return '还原基础油';
+                                break;
+                            case'gchysz': 
+                                return '高纯环氧树脂';
+                                break;
+                            case'gcdgz': 
+                                return '高纯单甘酯';
+                                break;
+                            // case'hyjcy': 
+                            //     return '还原基础油';
+                            //     break;
+                            // case'hyjcy': 
+                            //     return '还原基础油';
+                            //     break;
+                            // case'hyjcy': 
+                            //     return '还原基础油';
+                            //     break;
+                            // case'hyjcy': 
+                            //     return '还原基础油';
+                            //     break;
+                            // case'hyjcy': 
+                            //     return '还原基础油';
+                            //     break;
+                        }
+                    }
                 break;
                 case'text':
-                    return new News([
-                                    'title'       => '欢迎'.$userApi->get($message->FromUserName)->nickname.'访问广州市浩立生物科技有限公司！',
-                                    'description' => '浩立与华南理工大学共同携手合作，专业分子蒸馏、水蒸气蒸馏、超临界CO2萃取、超重力场等高新提纯、分离技术研究开发。致力于：天然产物、香料、化工材料等研发与应用；分离提纯设备、化工及香料生产设备、香料及化工等设备设计制造。',
-                                    'url'         => 'http://www.hao-li.net/',
-                                    'image'       => 'http://www.hao-li.net/Upload/PicFiles/2011.9.11_15.5.41_8683.jpg',
-                                    ]);
+                    return 'fsfs';
+                    // return new News([
+                    //                 'title'       => '欢迎'.$userApi->get($message->FromUserName)->nickname.'访问广州市浩立生物科技有限公司！',
+                    //                 'description' => '浩立与华南理工大学共同携手合作，专业分子蒸馏、水蒸气蒸馏、超临界CO2萃取、超重力场等高新提纯、分离技术研究开发。致力于：天然产物、香料、化工材料等研发与应用；分离提纯设备、化工及香料生产设备、香料及化工等设备设计制造。',
+                    //                 'url'         => 'http://www.hao-li.net/',
+                    //                 'image'       => 'http://www.hao-li.net/Upload/PicFiles/2011.9.11_15.5.41_8683.jpg',
+                    //                 ]);
                 break;
             };    
         });//自动回复
