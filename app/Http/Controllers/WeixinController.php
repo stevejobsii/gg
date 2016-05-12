0d25694f931e52526fade6416d7c66878bc9f3d7
di <stevejobsii@outlook.com>
2016-05-13 07:28:30 +0800

    test

--- app/Http/Controllers/WeixinController.php
+++ app/Http/Controllers/WeixinController.php
@@ -65,14 +65,13 @@ class WeixinController extends Controller
                 break;
                 case'text':
                     $transfer = new \EasyWeChat\Message\Transfer();
-                    $transfer->account('kf2001@better_technology');
+                    echo  new News([
+                                    'title'       => '欢迎'.$userApi->get($message->FromUserName)->nickname.'访问广州市浩立生物科技有限公司！',
+                                    'description' => '浩立与华南理工大学共同携手合作，专业分子蒸馏、水蒸气蒸馏、超临界CO2萃取、超重力场等高新提纯、分离技术研究开发。致力于：天然产物、香料、化工材料等研发与应用；分离提纯设备、化工及香料生产设备、香料及化工等设备设计制造。（向我们发送信息，我们将为您服务）',
+                                    'url'         => 'http://www.hao-li.net/',
+                                    'image'       => 'https://goodgoto.com/images/catalog/NBijDM.jpg',
+                                    ]);
                     return $transfer;
-                    // return new News([
-                    //                 'title'       => '欢迎'.$userApi->get($message->FromUserName)->nickname.'访问广州市浩立生物科技有限公司！',
-                    //                 'description' => '浩立与华南理工大学共同携手合作，专业分子蒸馏、水蒸气蒸馏、超临界CO2萃取、超重力场等高新提纯、分离技术研究开发。致力于：天然产物、香料、化工材料等研发与应用；分离提纯设备、化工及香料生产设备、香料及化工等设备设计制造。（向我们发送信息，我们将为您服务）',
-                    //                 'url'         => 'http://www.hao-li.net/',
-                    //                 'image'       => 'https://goodgoto.com/images/catalog/NBijDM.jpg',
-                    //                 ]);
                 break;
                 default:
                     break;
