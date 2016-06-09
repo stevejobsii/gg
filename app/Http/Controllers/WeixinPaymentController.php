50d64d2e993f5729bf7fa46a4bfc019c96b05095
di <stevejobsii@outlook.com>
2016-06-09 18:39:45 +0800

    test

--- app/Http/Controllers/WeixinPaymentController.php
+++ app/Http/Controllers/WeixinPaymentController.php
@@ -28,6 +28,7 @@ class WeixinPaymentController extends Controller
         ]);
         }
         Auth::login($user,true);
+
         $payment = $app->payment;
         $attributes = [
         'trade_type'       => 'JSAPI', // JSAPI，NATIVE，APP...
@@ -72,34 +73,35 @@ class WeixinPaymentController extends Controller
         //return $user->toArray();
     }  
 
-    // public function paymentnotify()
-    // {
-    //     $response = $app->payment->handleNotify(function($notify, $successful){
-    //         // 使用通知里的 "微信支付订单号" 或者 "商户订单号" 去自己的数据库找到订单
-    //         $order = 查询订单($notify->transaction_id);
+    public function paymentnotify()
+    {
+        return 'sfs';
+        $response = $app->payment->handleNotify(function($notify, $successful){
+            // 使用通知里的 "微信支付订单号" 或者 "商户订单号" 去自己的数据库找到订单
+            //$order = 查询订单($notify->transaction_id);
 
-    //         if (!$order) { // 如果订单不存在
-    //             return 'Order not exist.'; // 告诉微信，我已经处理完了，订单没找到，别再通知我了
-    //         }
+            if (!$order) { // 如果订单不存在
+                return 'Order not exist.'; // 告诉微信，我已经处理完了，订单没找到，别再通知我了
+            }
 
-    //         // 如果订单存在
-    //         // 检查订单是否已经更新过支付状态
-    //         if ($order->paid_at) { // 假设订单字段“支付时间”不为空代表已经支付
-    //             return true; // 已经支付成功了就不再更新了
-    //         }
+            // 如果订单存在
+            // 检查订单是否已经更新过支付状态
+            if ($order->paid_at) { // 假设订单字段“支付时间”不为空代表已经支付
+                return true; // 已经支付成功了就不再更新了
+            }
 
-    //         // 用户是否支付成功
-    //         if ($successful) {
-    //             // 不是已经支付状态则修改为已经支付状态
-    //             $order->paid_at = time(); // 更新支付时间为当前时间
-    //             $order->status = 'paid';
-    //         } else { // 用户支付失败
-    //             $order->status = 'paid_fail';
-    //         }
+            // 用户是否支付成功
+            if ($successful) {
+                // 不是已经支付状态则修改为已经支付状态
+                $order->paid_at = time(); // 更新支付时间为当前时间
+                $order->status = 'paid';
+            } else { // 用户支付失败
+                $order->status = 'paid_fail';
+            }
 
-    //         $order->save(); // 保存订单
+            $order->save(); // 保存订单
 
-    //         return true; // 返回处理完成
-    //     });
-    // }  
+            return true; // 返回处理完成
+        });
+    }  
 }
