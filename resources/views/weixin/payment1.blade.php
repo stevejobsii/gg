c7d158febbf4a576a4f2e6516d1e5bff5ece9eaf
di <stevejobsii@outlook.com>
2016-06-09 14:45:38 +0800

    teset

--- resources/views/weixin/payment1.blade.php
+++ resources/views/weixin/payment1.blade.php
@@ -84,16 +84,6 @@ document.querySelector('#onclickpay').onclick = function () {
         }
    ); 
 }
-if (typeof WeixinJSBridge == "undefined"){
-   if( document.addEventListener ){
-       document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
-   }else if (document.attachEvent){
-       document.attachEvent('WeixinJSBridgeReady', onBridgeReady); 
-       document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
-   }
-}else{
-   onBridgeReady();
-}
 </script>
 </body>
 </html>
